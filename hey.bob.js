65fb3ac8afcb845fcdd01e3a1bb59fdab0884efb
ph.charriere <ph.charriere@gmail.com>
2017-01-10 12:54:17 +0100

    :mushroom: fix ...

--- hey.bob.js
+++ hey.bob.js
@@ -6,9 +6,9 @@ require('shelljs/global');
 console.log("🤖 > I'm BoB, I'm starting the voice recognition... ")
 
 process.env.GCLOUD_PROJECT = "hey-bob-desktop";
-process.env.GOOGLE_APPLICATION_CREDENTIALS = "~/bob/bob.json";
-process.env.GOOGLE_RAW = `~/bob/google-voice-recognition/resources/bob.raw`;
+process.env.GOOGLE_APPLICATION_CREDENTIALS = "/home/pi/bob/bob.json";
+process.env.GOOGLE_RAW = `/home/pi/bob/google-voice-recognition/resources/bob.raw`;
 
 process.env.TRANSCRIPTION_RECEIVER_URL = "http://localhost:8080/transcription"
 
-exec(`node ~/bob/google-voice-recognition/reco.js`)
+exec(`node /home/pi/bob/google-voice-recognition/reco.js`)
