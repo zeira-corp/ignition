a1bbd504e819c74350985d691e342233a06e7273
Philippe Charrière <ph.charriere@gmail.com>
2017-01-10 12:42:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,24 @@
 
 require('shelljs/global');
 
-// start services
-//exec(`./bin/hubot -a rocketchat`)
+  let cmd = [
+    , `cd button-listener; `
+    , `sudo python listen.py;`
+  ].join('');
+
+  exec(cmd, (code, stdout, stderr) => {
+    console.log("wait for pushing button...")
+        if(code !== 0) {
+      console.error(`   button listener  `);
+        } else{
+          console.info('   button listener  ')
+        }
+  })
+
+  exec('node /home/pi/bob/simple-http/index.js', (code, stdout, stderr) => {
+
+  })
+
+
+
+  console.log("--- Bob is waiting... ---")
