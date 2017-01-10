ef40ba7633cc9f6735e1acbebca3a793f0d334da
ph.charriere <ph.charriere@gmail.com>
2017-01-10 12:51:29 +0100

    🌺 Gardening

--- index.js
+++ index.js
@@ -3,24 +3,22 @@
 
 require('shelljs/global');
 
-  let cmd = [
-    , `cd button-listener; `
-    , `sudo python listen.py;`
-  ].join('');
+let cmd = [
+  , `cd button-listener; `
+  , `sudo python listen.py;`
+].join('');
 
-  exec(cmd, (code, stdout, stderr) => {
-    console.log("wait for pushing button...")
-        if(code !== 0) {
-      console.error(`   button listener  `);
-        } else{
-          console.info('   button listener  ')
-        }
-  })
+exec(cmd, (code, stdout, stderr) => {
+  // foo
+  console.log(code)
+})
 
-  exec('node /home/pi/bob/simple-http/index.js', (code, stdout, stderr) => {
+// run http server to capture the transcription
+// soon, this will be hubot
+// 'node /home/pi/bob/simple-http/index.js'
+exec('node ~/bob/simple-http/index.js', (code, stdout, stderr) => {
+  // foo
+  console.log(code)
+})
 
-  })
-
-
-
-  console.log("--- Bob is waiting... ---")
+console.log("⌛️ Bob is waiting...")
